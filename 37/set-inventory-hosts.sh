7ec18b813d776675bb2d268142c3fdc52f56148c
gogs <gogs>
2018-04-27 15:05:41 -0400

    inventory-hosts

--- 37/set-inventory-hosts.sh
+++ 37/set-inventory-hosts.sh
@@ -1,3 +1,3 @@
-export INVENTORY_HOSTS=inventory-hosts-dns
-#export INVENTORY_HOSTS=inventory-hosts
+#export INVENTORY_HOSTS=inventory-hosts-dns
+export INVENTORY_HOSTS=inventory-hosts
 echo $INVENTORY_HOSTS
