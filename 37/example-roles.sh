b71a0f902cd85a3f3f4cf570fba838b14332fa61
gogs <gogs>
2018-04-27 16:58:09 -0400

    example

---
+++ 37/example-roles.sh
@@ -0,0 +1,24 @@
+#login as system admin first to the master
+oc adm policy add-role-to-user admin system -n default
+
+oc adm policy add-role-to-user admin system -n kube-public
+
+oc adm policy add-role-to-user admin system -n kube-service-catalog
+
+ oc adm policy add-role-to-user admin system -n kube-system
+
+ oc adm policy add-role-to-user admin system -n logging
+
+ oc adm policy add-role-to-user admin system -n management-infra
+
+ oc adm policy add-role-to-user admin system -n openshift
+
+ oc adm policy add-role-to-user admin system -n openshift-ansible-service-broker
+
+ oc adm policy add-role-to-user admin system -n openshift-infra
+
+ oc adm policy add-role-to-user admin system -n openshift-node
+
+ oc adm policy add-role-to-user admin system -n openshift-template-service-broker
+
+
