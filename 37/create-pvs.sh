07410d304f0e39a3ecb93478ccf0ee6c09b5cd19
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-21 19:36:42 +0000

    pvs

---
+++ 37/create-pvs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#. $(dirname $0)/install.conf
+
+#for i in {0..99}
+for i in {0..9}
+do
+	DIRNAME=$(printf "vol%02d" $i)
+	mkdir -p /mnt/data/$DIRNAME
+        chmod a+rwx /mnt/data/$DIRNAME
+	chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
+done
+
+sleep 5
+#oc login console.$DOMAIN:$API_PORT -u $ADMIN_USER -p $ADMIN_PASS
+
+#for i in {0..99}
+for i in {0..9}
+do
+	DIRNAME=$(printf "vol%02d" $i)
+	sed -i "s/name: vol..*/name: $DIRNAME/g" vol.yaml
+	sed -i "s/path: \/mnt\/data\/vol..*/path: \/mnt\/data\/$DIRNAME/g" vol.yaml
+	oc create -f vol.yaml
+	echo "created volume $i"
+done
