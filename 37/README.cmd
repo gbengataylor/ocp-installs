3a819aa88d54e7e2e3c259fe10159680e0026c6e
Gbenga Taylor <30447678+gbengataylor@users.noreply.github.com>
2018-05-24 09:13:02 -0400

    Update README.cmd

--- 37/README.cmd
+++ 37/README.cmd
@@ -1,3 +1,10 @@
+for 37, using https://github.com/openshift/openshift-ansible/tree/release-3.7/playbooks
+git clone https://github.com/openshift/openshift-ansible/tree/release-3.7/playbooks
+cd openshift-ansible
+git branch release-3.7
+cd ..
+
+
 order
 fix-control-node
 set-inventory-hosts
