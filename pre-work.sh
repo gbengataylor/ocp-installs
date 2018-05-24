c537959928f606c11df1d77ce1b01c86746671da
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-24 20:17:28 +0000

    fix prework

--- pre-work.sh
+++ pre-work.sh
@@ -1,2 +1,3 @@
-ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  create-export-dir.yaml ! [ $? == 0 ] && echo " CREATE EXPORT FAILED" && exit 1 
-ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml ! [ $? == 0 ] && echo "PREREQ FAILED" && exit 1
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  create-export-dir.yaml 
+! [ $? == 0 ] && echo " CREATE EXPORT FAILED" && exit 1 
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml 
