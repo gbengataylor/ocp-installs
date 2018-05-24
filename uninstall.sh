826c3a1f2053725cc5acdec22bc5753e4093b0e7
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-24 19:53:22 +0000

    fixed uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE openshift-ansible/playbooks/adhoc/uninstall.yml
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
