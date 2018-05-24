81bf6ca01be24a8c4d36250c032851650bebea7a
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-24 15:14:06 +0000

    changes

--- set-inventory-hosts.sh
+++ set-inventory-hosts.sh
@@ -1,3 +1,10 @@
+export MASTER=18.188.76.211.nip.io
+export INFRA=18.217.182.217.nip.io
+export COMPUTE=52.15.184.233.nip.io 
+
 #export INVENTORY_HOSTS=inventory-hosts-dns
 export INVENTORY_HOSTS=inventory-hosts
 echo $INVENTORY_HOSTS
+echo $MASTER
+echo $INFRA
+echo $COMPUTE
