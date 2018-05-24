d5167e93fdd215269a812ec19c2343d78280ebc5
Gbenga Taylor <30447678+gbengataylor@users.noreply.github.com>
2018-05-24 10:30:19 -0400

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,12 +1,21 @@
 Download RPMs
+----------------
 
 
 
-order
+More-prep
+---------------
 fix-control-node
 set-inventory-hosts
 ping-hosts
-prepare the hosts for the install using https://raw.githubusercontent.com/gbengataylor/ocp-ansible-playbooks/master/galaxy/prepare_cluster.yml
+
+prepare the hosts for the install using https://raw.githubusercontent.com/gbengataylor/ocp-ansible-playbooks/master/galaxy-39/prepare_cluster.yml
+git clone https://github.com/gbengataylor/ocp-ansible-playbooks.git
+cd ocp-ansible-playbooks/galaxy-39
+update the hosts file
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts prepare_cluster.yml --extra-vars "rhn_user={{ lookup ('env', 'RH_USER') }} rhn_pass={{ lookup ('env', 'RH_PASS') }} rhn_pool_id={{ lookup ('env', 'RH_POOL') }} ocp_repos=rhel-7-server-ose-3.9-rpms cluster_name=ocp-cluster"
+
+
 pre-work
 install
 
@@ -14,5 +23,7 @@ install
 example-roles
 create-pvs (not working)
 
+To uninstal
+----------------
 #uninstall
 uninstall
