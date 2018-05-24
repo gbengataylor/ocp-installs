805c864db2f7aa73c20f7f602dd12b0709ff16e2
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-24 17:04:29 +0000

    changes

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE openshift-ansible/playbooks/byo/config.yml
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
--- pre-work.sh
+++ pre-work.sh
@@ -1,1 +1,2 @@
- ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  create-export-dir.yaml
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  create-export-dir.yaml ! [ $? == 0 ] && echo " CREATE EXPORT FAILED" && exit 1 
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE  /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml ! [ $? == 0 ] && echo "PREREQ FAILED" && exit 1
