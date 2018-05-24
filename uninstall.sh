594b898174f698c31e8ed1b347a58ec2b41b8053
root <root@ip-172-31-37-248.us-east-2.compute.internal>
2018-05-24 20:06:32 +0000

    adding remove mounts

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,2 @@
 ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
+ansible-playbook -i $INVENTORY_HOSTS --private-key $AWS_PEM_FILE remove-mnts.yml
